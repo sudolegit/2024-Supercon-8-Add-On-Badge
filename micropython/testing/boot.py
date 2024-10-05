5ea8ecf39e169f0c7c82c8691550f3bfc1c184f7
Elliot Williams <elliot@pre.postero.us>
2024-10-05 23:50:33 +0200

    working test of the touchwheel and LED petal
    with optional support for an SSD1306 screen
    
    the problem was that the touchwheel doesn't respond until it's done with
    its startup animation, and my code was too impatient
    
    right now, waits for 5 sec to find it before giving up

--- micropython/testing/boot.py
+++ micropython/testing/boot.py
@@ -2,20 +2,23 @@
 
 from machine import I2C, Pin
 import time
+import ssd1306
 
-time.sleep(2.2)
+PETAL_ADDRESS      = 0x00
+TOUCHWHEEL_ADDRESS = 0x54
+SCREEN_ADDRESS     = 0x3C
 ## wrap this all up in a badge class eventually
 
 # Testing options
-HAS_SCREEN = True
-
-if HAS_SCREEN:
-    import ssd1306
 
 ## buttons
 buttonA = Pin(8, Pin.IN, Pin.PULL_UP)
 buttonB = Pin(9, Pin.IN, Pin.PULL_UP)
 buttonC = Pin(28, Pin.IN, Pin.PULL_UP)
+
+## debug pin!
+# p = Pin(7, Pin.OUT)
+# p.value(1)
 
 ## GPIOs
 
@@ -23,30 +26,86 @@ buttonC = Pin(28, Pin.IN, Pin.PULL_UP)
 i2c0 = I2C(0, sda=Pin(0), scl=Pin(1), freq=400_000)
 i2c1 = I2C(1, sda=Pin(26), scl=Pin(27), freq=400_000)
 
-def which_bus_has_device_id(i2c_id_string, debug=True):
+def which_bus_has_device_id(i2c_id, debug=False):
     '''Returns a list of i2c bus objects that have the requested id on them.
     Note this can be of length 0, 1, or 2 depending on which I2C bus the id is found'''
 
-    i2c0_bus = [hex(x) for x in i2c0.scan()] 
+    i2c0_bus = i2c0.scan() 
     if debug:
         print("Bus 0: ")
-        print(str(i2c0_bus))
+        print(str([hex(x) for x in i2c0_bus]))
 
-    i2c1_bus = [hex(x) for x in i2c1.scan()] 
+    i2c1_bus = i2c1.scan()
     if debug:
         print("Bus 1: ")
-        print(str(i2c1_bus))
+        print(str([hex(x) for x in i2c1_bus]))
 
     busses = []
-    if str(i2c_id_string) in i2c0_bus:
+    if i2c_id in i2c0_bus:
         busses.append(i2c0)
-    if str(i2c_id_string) in i2c1_bus:
+    if i2c_id in i2c1_bus:
         busses.append(i2c1)
 
     return(busses)
 
-## find first touchwheel
-touchwheel_bus =  which_bus_has_device_id("0x54")[0]
+## Look for an SSD1306 screen on the bus
+HAS_SCREEN = False
+busses = which_bus_has_device_id(SCREEN_ADDRESS)
+if len(busses) == 1:
+    screen = ssd1306.SSD1306_I2C(128, 64, busses[0])
+    HAS_SCREEN = True
+else: 
+    print(f"Warning: {len(busses)} busses found that match SSD1306 address.")
+
+def petal_init(bus):
+    """configure the petal SAO"""
+    bus.writeto_mem(PETAL_ADDRESS, 0x09, bytes([0x00]))  ## raw pixel mode (not 7-seg) 
+    bus.writeto_mem(PETAL_ADDRESS, 0x0A, bytes([0x09]))  ## intensity (of 16) 
+    bus.writeto_mem(PETAL_ADDRESS, 0x0B, bytes([0x07]))  ## enable all segments
+    bus.writeto_mem(PETAL_ADDRESS, 0x0C, bytes([0x81]))  ## undo shutdown bits 
+    bus.writeto_mem(PETAL_ADDRESS, 0x0D, bytes([0x00]))  ##  
+    bus.writeto_mem(PETAL_ADDRESS, 0x0E, bytes([0x00]))  ## no crazy features (default?) 
+    bus.writeto_mem(PETAL_ADDRESS, 0x0F, bytes([0x00]))  ## turn off display test mode 
+
+## can't use scan logic for petal b/c it's at address 0
+## so wrapping the init routine it try: blocks should also work
+## later on can test if petal_bus is None
+petal_bus = None
+try:
+    petal_init(i2c0)
+    petal_bus = i2c0
+except: 
+    pass
+try:
+    petal_init(i2c1)
+    petal_bus = i2c1
+except:
+    pass
+if not petal_bus:
+    print(f"Warning: Petal not found.")
+
+
+## waiting for wheel with a yellow light
+petal_bus.writeto_mem(PETAL_ADDRESS, 3, bytes([0x80]))
+petal_bus.writeto_mem(PETAL_ADDRESS, 2, bytes([0x80]))
+
+## touchwheel last, with a wait loop,  b/c it doesn't init until animation is over
+## probably need to implement a timeout here?
+touchwheel_bus = None
+touchwheel_counter = 0
+while not touchwheel_bus:
+    try:
+        touchwheel_bus =  which_bus_has_device_id(0x54)[0]
+    except:
+        pass
+    time.sleep_ms(100)
+    touchwheel_counter = touchwheel_counter + 1
+    if touchwheel_counter > 50:
+        break
+if not touchwheel_bus:
+    print(f"Warning: Touchwheel not found.")
+
+
 def touchwheel_read(bus):
     """Returns 0 for no touch, 1-255 clockwise around the circle from the south"""
     return(touchwheel_bus.readfrom_mem(84, 0, 1)[0])
@@ -58,30 +117,11 @@ def touchwheel_rgb(bus, r, g, b):
     touchwheel_bus.writeto_mem(84, 17, bytes([b]))
 
 
-def flower_init(bus):
-    """configure the petal SAO"""
-    bus.writeto_mem(0, 0x09, bytes([0x00]))  ## raw pixel mode (not 7-seg) 
-    bus.writeto_mem(0, 0x0A, bytes([0x09]))  ## intensity (of 16) 
-    bus.writeto_mem(0, 0x0B, bytes([0x07]))  ## enable all segments
-    bus.writeto_mem(0, 0x0C, bytes([0x81]))  ## undo shutdown bits 
-    bus.writeto_mem(0, 0x0D, bytes([0x00]))  ##  
-    bus.writeto_mem(0, 0x0E, bytes([0x00]))  ## no crazy features (default?) 
-    bus.writeto_mem(0, 0x0F, bytes([0x00]))  ## turn off display test mode 
-
-## one of these will fail out...
-try:
-    flower_init(i2c0)
-except: 
-    pass
-try:
-    flower_init(i2c1)
-except:
-    pass
-
-if HAS_SCREEN:
-    ## config screen
-    busses = which_bus_has_device_id("0x3c")
-    if len(busses) == 1:
-        screen = ssd1306.SSD1306_I2C(128, 64, busses[0])
+## goes green if wheel configured
+if touchwheel_bus:
+    petal_bus.writeto_mem(PETAL_ADDRESS, 3, bytes([0x00]))
+petal_bus.writeto_mem(PETAL_ADDRESS, 2, bytes([0x80]))
+time.sleep_ms(200)
+petal_bus.writeto_mem(PETAL_ADDRESS, 2, bytes([0x00]))
 
 
--- micropython/testing/main.py
+++ micropython/testing/main.py
@@ -3,37 +3,60 @@ import time
 
 while True:
 
-    screen.fill(0)
+    if HAS_SCREEN:
+        screen.fill(0)
 
-    screen.text("Hello Hackaday!", 10, 0)
+        screen.text("Hello Hackaday!", 10, 0)
 
-    screen.text("Button A: ", 25, 20)
-    screen.text(str(buttonA.value()), 100, 20)
-    screen.text("Button B: ", 25, 30)
-    screen.text(str(buttonB.value()), 100, 30)
-    screen.text("Button C: ", 25, 40)
-    screen.text(str(buttonC.value()), 100, 40)
+        screen.text("Button A: ", 25, 20)
+        screen.text(str(buttonA.value()), 100, 20)
+        screen.text("Button B: ", 25, 30)
+        screen.text(str(buttonB.value()), 100, 30)
+        screen.text("Button C: ", 25, 40)
+        screen.text(str(buttonC.value()), 100, 40)
 
-    screen.text("Wheel: ", 25, 50)
-    screen.text(str(touchwheel_read(touchwheel_bus)), 80, 50)
+        if touchwheel_bus:
+            screen.text("Wheel: ", 25, 50)
+            screen.text(str(touchwheel_read(touchwheel_bus)), 80, 50)
 
-    screen.show()
-
-    tw = touchwheel_read(touchwheel_bus)
-    print(tw)
-
-    ## display on petal
-    if tw > 0:
-        petal = int(tw/32) + 1
-    else: 
-        petal = 999
-    for i in range(1,9):
-        if i == petal:
-            i2c0.writeto_mem(0, i, bytes([0x7F]))
-        else:
-            i2c0.writeto_mem(0, i, bytes([0x00]))
+        screen.show()
     
-    time.sleep_ms(100)
+    ## display button status on RGB
+    if petal_bus:
+        if not buttonA.value():
+            petal_bus.writeto_mem(PETAL_ADDRESS, 2, bytes([0x80]))
+        else:
+            petal_bus.writeto_mem(PETAL_ADDRESS, 2, bytes([0x00]))
+
+        if not buttonB.value():
+            petal_bus.writeto_mem(PETAL_ADDRESS, 3, bytes([0x80]))
+        else:
+            petal_bus.writeto_mem(PETAL_ADDRESS, 3, bytes([0x00]))
+
+        if not buttonC.value():
+            petal_bus.writeto_mem(PETAL_ADDRESS, 4, bytes([0x80]))
+        else:
+            petal_bus.writeto_mem(PETAL_ADDRESS, 4, bytes([0x00]))
+
+    ## see what's going on with the touch wheel
+    if touchwheel_bus:
+        tw = touchwheel_read(touchwheel_bus)
+
+    ## display touchwheel on petal
+    if petal_bus and touchwheel_bus:
+        if tw > 0:
+            petal = int(tw/32) + 1
+        else: 
+            petal = 999
+        for i in range(1,9):
+            if i == petal:
+                petal_bus.writeto_mem(0, i, bytes([0x7F]))
+            else:
+                petal_bus.writeto_mem(0, i, bytes([0x00]))
+
+
+    
+    time.sleep_ms(20)
 
 
 
